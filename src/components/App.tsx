c6134a7a4fc82fa144a943d6a80a4c1885a3fe0a
trancore <ki.program0915@gmail.com>
2023-07-30 16:52:14 +0900

    first commit

---
+++ src/components/App.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from "react";
+import "~/components/App.modules.scss";
+import { Layout } from "~/components/common/Layout";
+
+export const App: FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Layout>🚧工事中。。。</Layout>
+      </header>
+    </div>
+  );
+};
---
+++ src/components/common/Layout.tsx
@@ -0,0 +1,9 @@
+import React, { FC, ReactNode } from "react";
+
+type Props = {
+  children: ReactNode;
+};
+
+export const Layout: FC<Props> = ({ children }) => {
+  return <>{children}</>;
+};
