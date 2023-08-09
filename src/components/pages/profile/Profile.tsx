74529a39ef5c1911a2754c689c582a2e5a6fcc21
trancore <ki.program0915@gmail.com>
2023-08-10 00:17:06 +0900

    add: Profile page template

---
+++ src/components/pages/profile/Profile.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from "react";
+
+export const Profile: FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Profile</p>
+        <p>只今作成中です。</p>
+      </header>
+    </div>
+  );
+};
