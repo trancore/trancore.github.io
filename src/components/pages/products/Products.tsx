296e9a2870b11dc63c4a004181be1e8f15040411
trancore <ki.program0915@gmail.com>
2023-08-10 00:17:20 +0900

    add: Products page template

---
+++ src/components/pages/products/Products.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from "react";
+
+export const Products: FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Products</p>
+        <p>只今作成中です。</p>
+      </header>
+    </div>
+  );
+};
