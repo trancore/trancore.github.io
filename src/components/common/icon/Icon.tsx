77124056414261a67907688f6c86c03f9116993a
trancore <ki.program0915@gmail.com>
2023-08-16 23:47:15 +0900

    feature: feature icon size

--- src/components/common/icon/Icon.tsx
+++ src/components/common/icon/Icon.tsx
@@ -4,7 +4,7 @@ import { ReactComponent as Music } from "~/assets/images/icon/music_64.svg";
 import { ReactComponent as Person } from "~/assets/images/icon/person_64.svg";
 
 const ICONS = { Home, Music, Person };
-const DEFAULT_ICON_SIZE = 64;
+const DEFAULT_ICON_SIZE = 44;
 
 type IconName = keyof typeof ICONS;
 type IconSize = 24 | typeof DEFAULT_ICON_SIZE;
