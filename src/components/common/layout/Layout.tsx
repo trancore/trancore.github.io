4a8c62899f471615b0d1d0c3d7f505fe207c862f
trancore <ki.program0915@gmail.com>
2023-08-19 17:33:08 +0900

    feature: feature top page's header markup

--- src/components/common/layout/Layout.tsx
+++ src/components/common/layout/Layout.tsx
@@ -2,15 +2,18 @@ import React from "react";
 
 import { Outlet, useLocation } from "react-router-dom";
 
+import { PATH } from "~/const";
+
 import { Header } from "~/components/common/header/Header";
 
+import "~/components/common/layout/Layout.modules.scss";
+
 export const Layout: React.FC = () => {
-  const location = useLocation();
-  console.log("🚀 ~ file: Layout.tsx:13 ~ location:", location);
+  const { pathname } = useLocation();
 
   return (
     <div className="page">
-      <Header />
+      {pathname !== PATH.TOP && <Header />}
       <main>
         <Outlet />
       </main>
