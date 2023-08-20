d1256dfbe127e0ba8d21913b469f556ac18729c0
trancore <ki.program0915@gmail.com>
2023-08-20 16:14:22 +0900

    feature: feature Products page markup

--- src/components/pages/products/Products.tsx
+++ src/components/pages/products/Products.tsx
@@ -1,12 +1,33 @@
-import React, { FC } from "react";
+import React, { ComponentProps, FC } from "react";
+
+import { Repository } from "~/components/ui/repository/Repository";
+
+import classes from "~/components/pages/products/Products.module.scss";
 
 export const Products: FC = () => {
+  const dummyList: ComponentProps<typeof Repository>["repositry"][] = [
+    {
+      readme: "README",
+      name: "trancore/trancore.github.io",
+      description: "My portfolio site.",
+      codeLanguage: "Typescript",
+    },
+    {
+      readme: "README",
+      name: "trancore/trancore.github.io",
+      description: "My portfolio site.",
+      codeLanguage: "Typescript",
+    },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Products</p>
-        <p>只今作成中です。</p>
-      </header>
+    <div className={classes.content}>
+      <p className={classes.title}>Products</p>
+      <div className={classes["repository-box"]}>
+        {dummyList.map((dummy) => (
+          <Repository key={dummy.name} repositry={dummy} />
+        ))}
+      </div>
     </div>
   );
 };
