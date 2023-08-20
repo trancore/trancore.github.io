11b0bd67a87d2746c8927cffff629bb0413b6608
trancore <ki.program0915@gmail.com>
2023-08-20 16:14:46 +0900

    featurer: feature Repository component props

--- src/components/ui/repository/Repository.tsx
+++ src/components/ui/repository/Repository.tsx
@@ -4,28 +4,37 @@ import { Icon } from "~/components/common/icon/Icon";
 
 import classes from "~/components/ui/repository/Repository.module.scss";
 
-export const Repository: React.FC = () => {
+type Props = {
+  repositry: {
+    readme: string;
+    name: string;
+    description: string;
+    codeLanguage: string;
+  };
+};
+
+export const Repository: React.FC<Props> = ({ repositry }) => {
   return (
     <div className={classes.repository}>
       <div className={classes.readme}>
         <div className={classes.readme_back}>
           <div className={classes.readme_content}>
-            <p>README</p>
+            <p>{repositry.readme}</p>
           </div>
         </div>
       </div>
       <div className={classes.description}>
         <div className={classes.repository_name}>
           <Icon name="Book" size={44}></Icon>
-          <p>trancore/trancore.github.io</p>
+          <p>{repositry.name}</p>
         </div>
         <div className={classes.repository_description}>
-          <p>My portfolio site.</p>
+          <p>{repositry.name}</p>
         </div>
         <div className={classes.repository_code}>
           <div className={classes.code_color}>⚪️</div>
           <div className={classes.code_language}>
-            <p>Typescript</p>
+            <p>{repositry.codeLanguage}</p>
           </div>
         </div>
       </div>
