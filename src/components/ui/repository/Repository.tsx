7fc1f36b2804abcb2a2c1a582ccb390f6bf576f7
trancore <ki.program0915@gmail.com>
2023-08-20 16:18:14 +0900

    feature: feature Repository components css name

--- src/components/ui/repository/Repository.tsx
+++ src/components/ui/repository/Repository.tsx
@@ -17,23 +17,23 @@ export const Repository: React.FC<Props> = ({ repositry }) => {
   return (
     <div className={classes.repository}>
       <div className={classes.readme}>
-        <div className={classes.readme_back}>
-          <div className={classes.readme_content}>
+        <div className={classes["readme-back"]}>
+          <div className={classes["readme-content"]}>
             <p>{repositry.readme}</p>
           </div>
         </div>
       </div>
       <div className={classes.description}>
-        <div className={classes.repository_name}>
+        <div className={classes["repository-name"]}>
           <Icon name="Book" size={44}></Icon>
           <p>{repositry.name}</p>
         </div>
-        <div className={classes.repository_description}>
+        <div className={classes["repository-description"]}>
           <p>{repositry.name}</p>
         </div>
-        <div className={classes.repository_code}>
-          <div className={classes.code_color}>⚪️</div>
-          <div className={classes.code_language}>
+        <div className={classes["repository-code"]}>
+          <div className={classes["code-color"]}>⚪️</div>
+          <div className={classes["code-language"]}>
             <p>{repositry.codeLanguage}</p>
           </div>
         </div>
