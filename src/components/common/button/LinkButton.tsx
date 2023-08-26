b4e4b25deedb1a88f9a24a12994877186e7c72fe
trancore <ki.program0915@gmail.com>
2023-08-26 17:28:26 +0900

    feature: feature link event in Top Component

--- src/components/common/button/LinkButton.tsx
+++ src/components/common/button/LinkButton.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 
+import { useNavigate } from "react-router-dom";
+
+import { PAGE_PATH } from "~/const";
+
 import classes from "~/components/common/button/LinkButton.module.scss";
 
 const DEFAULT_TEXT_SIZE = 12;
@@ -12,20 +16,28 @@ type Props = {
   text: string;
   buttonWidth?: ButtonWidth;
   textSize?: TextSize;
+  to: (typeof PAGE_PATH)[keyof typeof PAGE_PATH];
 };
 
 export const LinkButton: React.FC<Props> = ({
   buttonWidth = DEFAULT_BUTTON_WIDTH,
   text,
   textSize = DEFAULT_TEXT_SIZE,
+  to,
 }) => {
+  const navigate = useNavigate();
+
   const linkButtonStyle: React.CSSProperties = {
     width: buttonWidth,
     fontSize: textSize,
   };
 
   return (
-    <div className={classes["link-button"]} style={linkButtonStyle}>
+    <div
+      className={classes["link-button"]}
+      style={linkButtonStyle}
+      onClick={() => navigate(to)}
+    >
       <p className={classes["link-button-text"]}>{text}</p>
     </div>
   );
