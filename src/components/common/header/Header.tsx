5e7efc5a876370b903e7e6d9e0886534276a7fc4
trancore <ki.program0915@gmail.com>
2023-08-26 17:27:26 +0900

    feature: feature link event in Header component

--- src/components/common/header/Header.tsx
+++ src/components/common/header/Header.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 
+import { Link } from "react-router-dom";
+
+import { PAGE_PATH } from "~/const";
+
 import { Icon } from "~/components/common/icon/Icon";
 
 import classes from "~/components/common/header/Header.module.scss";
@@ -8,9 +12,15 @@ export const Header: React.FC = () => {
   return (
     <div className={classes.header}>
       <div className={classes.list}>
-        <Icon name="Home" />
-        <Icon name="Person" />
-        <Icon name="Music" />
+        <Link to={PAGE_PATH.TOP}>
+          <Icon name="Home" />
+        </Link>
+        <Link to={PAGE_PATH.PROFILE}>
+          <Icon name="Person" />
+        </Link>
+        <Link to={PAGE_PATH.PRODUCTS}>
+          <Icon name="Products" />
+        </Link>
       </div>
     </div>
   );
