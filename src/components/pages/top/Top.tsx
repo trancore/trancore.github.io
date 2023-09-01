7a06cecccdcfba7c93c4d0a714379b578ab3b4ff
trancore <ki.program0915@gmail.com>
2023-09-02 00:00:33 +0900

    feature: feature animation for Top component

--- src/components/pages/top/Top.tsx
+++ src/components/pages/top/Top.tsx
@@ -10,9 +10,9 @@ import { LinkButton } from "~/components/common/button/LinkButton";
 
 import classes from "~/components/pages/top/Top.module.scss";
 
-const BUTTON = {
+const BUTTON_PROPERtY = {
   TEXT_SIZE: 20,
-  WIDTH: { PC: 250, SP: 350 },
+  WIDTH: { PC: 350, SP: 250 },
   LIST: [
     { TEXT: "Profile", TO: PAGE_PATH.PROFILE },
     { TEXT: "Products", TO: PAGE_PATH.PRODUCTS },
@@ -20,27 +20,56 @@ const BUTTON = {
   ],
 } as const;
 
+const DEFAULT_DELAY_SECOND = 0.5;
+
 export const Top: React.FC = () => {
   const { isSp } = useMediaQuery();
+
+  const animateProps = { y: [50, 0], opacity: [0, 1] };
+
+  const getTransitionProps = (delay: number) => {
+    return { ease: "easeIn", duration: 1.0, delay: delay };
+  };
 
   return (
     <div className={classes.content}>
       <div className={classes["left-content"]}>
-        <p className={classes.welcome}>welcome</p>
-        <img className={classes["nazo-image"]} src={geometry68} />
+        <SimpleAnimation
+          componentType="p"
+          animateProps={animateProps}
+          transitionProps={getTransitionProps(0)}
+        >
+          welcome
+        </SimpleAnimation>
+        <SimpleAnimation
+          componentType="div"
+          animateProps={animateProps}
+          transitionProps={getTransitionProps(DEFAULT_DELAY_SECOND)}
+        >
+          <img className={classes["nazo-image"]} src={geometry68} />
+        </SimpleAnimation>
       </div>
       <div className={classes["right-content"]}>
         <ul>
-          {BUTTON.LIST.map((button) => {
+          {BUTTON_PROPERtY.LIST.map((button, index) => {
             return (
-              <li key={button.TEXT}>
+              <SimpleAnimation
+                key={button.TEXT}
+                componentType="li"
+                animateProps={animateProps}
+                transitionProps={getTransitionProps(
+                  DEFAULT_DELAY_SECOND + (index + 1) * DEFAULT_DELAY_SECOND,
+                )}
+              >
                 <LinkButton
                   text={button.TEXT}
-                  buttonWidth={isSp ? BUTTON.WIDTH.SP : BUTTON.WIDTH.PC}
-                  textSize={BUTTON.TEXT_SIZE}
+                  buttonWidth={
+                    isSp ? BUTTON_PROPERtY.WIDTH.SP : BUTTON_PROPERtY.WIDTH.PC
+                  }
+                  textSize={BUTTON_PROPERtY.TEXT_SIZE}
                   to={button.TO}
                 ></LinkButton>
-              </li>
+              </SimpleAnimation>
             );
           })}
         </ul>
