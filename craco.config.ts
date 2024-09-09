e82560f0a3817e067fd2b42b3e2c88dee4e05579
trancore <ki.program0915@gmail.com>
2024-09-09 23:14:20 +0900

    feature: add terser-plugin

--- craco.config.ts
+++ craco.config.ts
@@ -1,12 +1,19 @@
 import { resolve as _resolve } from "path";
+import TerserPlugin from "terser-webpack-plugin";
 
 module.exports = {
+  typescript: {
+    enableTypeChecking: true,
+  },
   webpack: {
     alias: {
       "~": _resolve(__dirname, "./src/"),
     },
-    configure: (webpackConfig: unknown) => {
-      return webpackConfig;
+    configure: {
+      optimization: {
+        minimize: true,
+        minimizer: [new TerserPlugin()],
+      },
     },
   },
 };
