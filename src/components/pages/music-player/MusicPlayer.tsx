9efd95ecad0fdfe9dbe74cd0db78a56c4ead983a
trancore <ki.program0915@gmail.com>
2024-09-21 19:04:25 +0900

    feature: hide header when locationing /music-player

--- src/components/pages/music-player/MusicPlayer.tsx
+++ src/components/pages/music-player/MusicPlayer.tsx
@@ -4,9 +4,22 @@ import classes from "~/components/pages/music-player/MusicPlayer.module.scss";
 
 export const MusicPlayer: FC = () => {
   return (
-    <div className={classes.content}>
-      <p>🎵</p>
-      <p>只今作成中です。</p>
+    <div>
+      <div>
+        <div>
+          <div>ジャケット画像</div>
+        </div>
+        <div>
+          <div>シークバー</div>
+          <div>Artist - 曲名</div>
+          <div>
+            <div>前の曲へ</div>
+            <div>再生 / 一時停止</div>
+            <div>次の曲へ</div>
+          </div>
+        </div>
+      </div>
+      <div>音楽リスト</div>
     </div>
   );
 };
