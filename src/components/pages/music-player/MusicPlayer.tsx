15fb522320d8c621f06a12c08e1e7ac1d85a3084
trancore <ki.program0915@gmail.com>
2024-09-23 21:02:17 +0900

    feature: add MusicPlayer page

--- src/components/pages/music-player/MusicPlayer.tsx
+++ src/components/pages/music-player/MusicPlayer.tsx
@@ -1,25 +1,115 @@
 import { FC } from "react";
 
+import { Link } from "react-router-dom";
+
+import { PAGE_PATH } from "~/const";
+
+import { Icon } from "~/components/common/icon/Icon";
+import { MusicTable } from "~/components/common/table/MusicTable";
+
 import classes from "~/components/pages/music-player/MusicPlayer.module.scss";
 
 export const MusicPlayer: FC = () => {
+  const musicList = [
+    {
+      artist: "kosuke iwasaki",
+      title: "test test test",
+      length: "20:00",
+    },
+    {
+      artist: "kosuke iwasaki",
+      title: "test test test",
+      length: "20:00",
+    },
+    {
+      artist: "kosuke iwasaki",
+      title: "test test test",
+      length: "20:00",
+    },
+    {
+      artist: "kosuke iwasaki",
+      title: "test test test",
+      length: "20:00",
+    },
+    {
+      artist: "kosuke iwasaki",
+      title: "test test test",
+      length: "20:00",
+    },
+    {
+      artist: "kosuke iwasaki",
+      title: "test test test",
+      length: "20:00",
+    },
+    {
+      artist: "kosuke iwasaki",
+      title: "test test test",
+      length: "20:00",
+    },
+    {
+      artist: "kosuke iwasaki",
+      title: "test test test",
+      length: "20:00",
+    },
+    {
+      artist: "kosuke iwasaki",
+      title: "test test test",
+      length: "20:00",
+    },
+    {
+      artist: "kosuke iwasaki",
+      title: "test test test",
+      length: "20:00",
+    },
+    {
+      artist: "kosuke iwasaki",
+      title: "test test test",
+      length: "20:00",
+    },
+    {
+      artist: "kosuke iwasaki",
+      title: "test test test",
+      length: "20:00",
+    },
+  ];
+
   return (
-    <div>
-      <div>
-        <div>
-          <div>ジャケット画像</div>
+    <div className={classes["music-player"]}>
+      <header className={classes.header}>
+        <Link to={PAGE_PATH.TOP}>
+          <Icon name="ArrowLeft" size={32}></Icon>
+        </Link>
+        <div className={classes["input-file"]}>
+          <input id="fileElement" type="file" multiple></input>
+          <button id="fileSelect" type="button">
+            ファイル選択
+          </button>
         </div>
-        <div>
-          <div>シークバー</div>
-          <div>Artist - 曲名</div>
-          <div>
-            <div>前の曲へ</div>
-            <div>再生 / 一時停止</div>
-            <div>次の曲へ</div>
+      </header>
+      <div className={classes.content}>
+        <div className={classes["music-meta"]}>
+          <div className={classes["jacket-picture"]}></div>
+          <div className={classes["player-control"]}>
+            <div className={classes.player}>
+              <div className={classes.time}>
+                <p>0:00</p>
+                <input className={classes.seekbar} type="range"></input>
+                <p>999:99</p>
+              </div>
+              <p>Artist - 曲名</p>
+              <div className={classes.control}>
+                <Icon name="Backforward" size={24}></Icon>
+                <span className={classes.play}>
+                  <Icon name="Fish" size={44}></Icon>
+                  {/* <Icon name="Resume" size={44}></Icon> */}
+                </span>
+                <Icon name="Forward" size={24}></Icon>
+              </div>
+            </div>
+            <MusicTable musicList={musicList}></MusicTable>
           </div>
         </div>
       </div>
-      <div>音楽リスト</div>
     </div>
   );
 };
