5d9103e63e3ac5f0f9d7cedb1250ff9b2f8d871c
trancore <ki.program0915@gmail.com>
2024-10-26 23:46:13 +0900

    feature: update music metadata type

--- src/types/Music.ts
+++ src/types/Music.ts
@@ -1,8 +1,22 @@
 ﻿/**
  * 音楽メタデータ用型定義
  */
+
+/**
+ * 音楽メタデータ
+ */
 export type Metadata = {
   artist: string;
   title: string;
+  artists: string;
+  album: string;
+  albumArtists: string;
+  genre: string;
+};
+
+/**
+ * 表示用音楽メタデータ
+ */
+export type DisplayMetadata = Pick<Metadata, "title" | "artist"> & {
   length: string;
 };
