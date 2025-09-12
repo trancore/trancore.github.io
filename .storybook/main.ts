cb0f79166d1bc44c0c00eb5a6f99e8fe79aa2c8f
trancore <ki.program0915@gmail.com>
2025-09-13 00:40:18 +0900

    feature: add storybook

---
+++ .storybook/main.ts
@@ -0,0 +1,22 @@
+import type { StorybookConfig } from "@storybook/react-vite";
+import path from "node:path";
+
+const config: StorybookConfig = {
+  stories: ["../src/components/**/*.stories.tsx"],
+  addons: ["@storybook/addon-a11y"],
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
+  viteFinal: async (config) => {
+    if (config.resolve) {
+      config.resolve.alias = {
+        ...config.resolve?.alias,
+        "~": path.resolve(__dirname, "../src"),
+      };
+    }
+
+    return config;
+  },
+};
+export default config;
