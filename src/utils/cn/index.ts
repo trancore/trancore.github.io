21fb96d786542e9b2c91b49fffa33cf8aa26f3e0
trancore <ki.program0915@gmail.com>
2025-09-24 22:50:35 +0900

    feature: add cn util

---
+++ src/utils/cn/index.ts
@@ -0,0 +1,16 @@
+﻿import clsx, { type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+/**
+ * クラス名管理カスタムヘルパー関数。
+ * 引数の順番に応じてグルーピングされたスタイルが定義される。
+ *
+ * 第1引数：
+ * 末端引数：オブジェクトとし、条件分岐によって変わるスタイル
+ *
+ * @param inputs クラス名
+ * @returns マージされたクラス名
+ */
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
