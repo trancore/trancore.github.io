cc287cb7e8c819021ae1df8c27c3fedf95ff5a3c
trancore <ki.program0915@gmail.com>
2025-09-27 22:26:50 +0900

    feature: add pointer: cursor in tab component

--- src/components/common/Tab/index.tsx
+++ src/components/common/Tab/index.tsx
@@ -10,7 +10,7 @@ type Props = {
 export default function Tab({ text, isActive = false, onClick }: Props) {
   return (
     <HeadlessTab
-      className={cn("py-2", "font-bold", {
+      className={cn("py-2", "font-bold", "cursor-pointer", {
         "border-b-2": isActive,
       })}
       onClick={onClick}
