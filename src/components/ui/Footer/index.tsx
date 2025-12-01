a27d47431da5ecb9cf207ac70b1d59538e6ee76a
trancore <ki.program0915@gmail.com>
2025-12-01 22:43:58 +0900

    feature: add Footer component

---
+++ src/components/ui/Footer/index.tsx
@@ -0,0 +1,11 @@
+﻿import { cn } from "~/utils/cn";
+
+export default function Footer() {
+  return (
+    <footer className={cn("py-5 w-full", "border-t-2 border-t-gray-200")}>
+      <p className={cn("flex justify-center", "text-gray-500 text-sm")}>
+        ©︎ 2025 Kosuke Iwasaki
+      </p>
+    </footer>
+  );
+}
