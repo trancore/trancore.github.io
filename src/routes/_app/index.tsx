b02d9892415e20b1cc69525eeffaeed41a68f2f7
trancore <ki.program0915@gmail.com>
2025-12-03 23:02:55 +0900

    feature: update routes

---
+++ src/routes/_app/index.tsx
@@ -0,0 +1,9 @@
+﻿import { createFileRoute } from "@tanstack/react-router";
+
+export const Route = createFileRoute("/_app/")({
+  component: Home,
+});
+
+function Home() {
+  return <div>Home</div>;
+}
