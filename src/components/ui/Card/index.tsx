f834c0812ce5b0dcbb739287289e75b16ca85873
trancore <ki.program0915@gmail.com>
2025-12-18 22:24:02 +0900

    feature: update Card design

--- src/components/ui/Card/index.tsx
+++ src/components/ui/Card/index.tsx
@@ -17,34 +17,38 @@ export default function Card({ card }: Props) {
   return (
     <div
       className={cn(
-        "max-w-xs p-4",
+        "h-[300px] max-w-xs p-4",
+        "flex flex-col justify-between",
         "rounded-lg border-2 border-gray-100 shadow-xl",
       )}
     >
-      <div className={cn("mb-4", "flex justify-center")}>
-        {hasEmoji && <p className={cn("text-4xl")}>{getRandomEmoji()}</p>}
-        {imgSrc && (
-          <img
-            alt="画像"
-            src={imgSrc}
-            height={500}
-            width={300}
-            className={cn("")}
-          />
-        )}
+      <div>
+        <div className={cn("mb-4", "flex justify-center")}>
+          {hasEmoji && <p className={cn("text-4xl")}>{getRandomEmoji()}</p>}
+          {imgSrc && (
+            <img
+              alt="画像"
+              src={imgSrc}
+              height={128}
+              className={cn("h-32", "aspect-auto object-contain")}
+            />
+          )}
+        </div>
+        <p className={cn("mb-2", "font-bold", "line-clamp-1")}>{title}</p>
+        <p className={cn("mb-2", "text-gray-500 text-sm", "line-clamp-3")}>
+          {description}
+        </p>
       </div>
-      <p className={cn("mb-2", "font-bold", "line-clamp-1")}>{title}</p>
-      <p className={cn("mb-2", "text-gray-500 text-sm", "line-clamp-3")}>
-        {description}
-      </p>
-      <a
-        href={url}
-        target="_blank"
-        rel="noopener noreferrer"
-        className={cn("font-bold text-blue-500 hover:underline")}
-      >
-        ＞＞＞
-      </a>
+      <div>
+        <a
+          href={url}
+          target="_blank"
+          rel="noopener noreferrer"
+          className={cn("font-bold text-blue-500 hover:underline")}
+        >
+          ＞＞＞
+        </a>
+      </div>
     </div>
   );
 }
