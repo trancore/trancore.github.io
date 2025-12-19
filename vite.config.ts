21498f7483fd7ee9625cb3c81a444a41b33280c3
trancore <ki.program0915@gmail.com>
2025-12-19 23:01:30 +0900

    feature: add vite-plugins-svgr

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
 /// <reference types="vitest" />
 import { resolve } from "node:path";
+
 import tailwindcss from "@tailwindcss/vite";
 import tanstackRouter from "@tanstack/router-plugin/vite";
 import viteReact from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+
+import svgr from "vite-plugin-svgr";
+import { defineConfig } from "vitest/config";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,6 +14,7 @@ export default defineConfig({
     tanstackRouter({ target: "react", autoCodeSplitting: true }),
     viteReact(),
     tailwindcss(),
+    svgr(),
   ],
   resolve: {
     alias: {
