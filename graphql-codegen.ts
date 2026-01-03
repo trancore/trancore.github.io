dca2aecbb2bac235c4cfced40ab526b5bcdde631
trancore <ki.program0915@gmail.com>
2026-01-03 23:07:11 +0900

    feature: update settings for graphql

---
+++ graphql-codegen.ts
@@ -0,0 +1,21 @@
+import type { CodegenConfig } from "@graphql-codegen/cli";
+
+const config: CodegenConfig = {
+  overwrite: true,
+  schema: [
+    {
+      "src/graphql/github/schema.docs.graphql": {},
+    },
+  ],
+  /** クエリ保存場所 */
+  documents: "src/graphql/github/**/*.graphql",
+  /** 生成先パス */
+  generates: {
+    "src/graphql/github/query-generated/": {
+      preset: "client",
+      plugins: [],
+    },
+  },
+};
+
+export default config;
