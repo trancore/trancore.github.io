f4392e99dce9b60d20f616e85923f9559d1d6f1d
trancore <ki.program0915@gmail.com>
2026-01-03 21:42:11 +0900

    feature: add menuSide component

---
+++ src/components/ui/MenuSide/index.stories.tsx
@@ -0,0 +1,30 @@
+﻿import type { Meta, StoryObj } from "@storybook/react-vite";
+
+import MenuSide from "~/components/ui/MenuSide";
+
+const meta = {
+  component: MenuSide,
+  parameters: {
+    layout: "centered",
+  },
+} satisfies Meta<typeof MenuSide>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    menuItems: [
+      {
+        title: "GitHub",
+      },
+      {
+        title: "music player",
+      },
+      {
+        title: "LP",
+        items: ["test1", "test2", "test3", "test4", "test5"],
+      },
+    ],
+  },
+};
---
+++ src/components/ui/MenuSide/index.tsx
@@ -0,0 +1,68 @@
+﻿import {
+  Disclosure,
+  DisclosureButton,
+  DisclosurePanel,
+} from "@headlessui/react";
+
+import Icon from "~/components/common/Icon";
+import { cn } from "~/utils/cn";
+
+type Props = {
+  menuItems: {
+    title: string;
+    items?: string[];
+  }[];
+};
+
+export default function MenuSide({ menuItems }: Props) {
+  return (
+    <aside
+      className={cn(
+        "p-6",
+        "flex flex-col items-start",
+        "rounded-lg border-2 border-gray-100 shadow-xl",
+      )}
+    >
+      {menuItems.map(({ title, items }) => (
+        <DisclosureItem key={title} title={title} items={items} />
+      ))}
+    </aside>
+  );
+}
+
+type DisclosureItemProps = Pick<Props["menuItems"][number], "title" | "items">;
+
+function DisclosureItem({ title, items }: DisclosureItemProps) {
+  return (
+    <Disclosure>
+      {({ open }) => (
+        <>
+          <DisclosureButton className={cn("py-2", "cursor-pointer font-bold")}>
+            <div className={cn("gap-2", "flex items-center")}>
+              {title}
+              {items &&
+                items.length > 0 &&
+                (open ? (
+                  <Icon type="CHEVRON_UP" size={16} />
+                ) : (
+                  <Icon type="CHEVRON_DOWN" size={16} />
+                ))}
+            </div>
+          </DisclosureButton>
+          {items && items.length > 0 && (
+            <DisclosurePanel
+              className={cn("py-2 pl-2", "cursor-pointer")}
+              as="ul"
+            >
+              {items.map((item) => (
+                <li className={cn("py-1", "cursor-pointer")} key={item}>
+                  {item}
+                </li>
+              ))}
+            </DisclosurePanel>
+          )}
+        </>
+      )}
+    </Disclosure>
+  );
+}
