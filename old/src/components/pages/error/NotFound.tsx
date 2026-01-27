0f0c06765f9cf88967cf94e25756db86cc15efd9
trancore <ki.program0915@gmail.com>
2026-01-27 22:47:43 +0900

    feature: non-requirement code

--- old/src/components/pages/error/NotFound.tsx
+++
@@ -1,7 +0,0 @@
-import { FC } from "react";
-
-import { Error } from "~/components/common/error/Error";
-
-export const NotFound: FC = () => {
-  return <Error errorStatus={404}></Error>;
-};
--- old/src/const/page.ts
+++
@@ -1,9 +0,0 @@
-/**
- * ページのパスの定数
- */
-export const PAGE_PATH = {
-  TOP: "/",
-  PROFILE: "/profile",
-  PRODUCTS: "/products",
-  MUSIC_PLAYER: "/music-player",
-} as const;
